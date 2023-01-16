0ba3c9a163084b09d2089d7d04dde462f726b98b
oleeriksen <oleeriksen1958@gmail.com>
2023-01-16 13:41:16 +0100

    first

---
+++ awaittest/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        run();
+        Console.WriteLine("After run...");
+        Console.ReadKey();
+
+    }
+
+    static Task GetTask(int amount, string text)
+    {
+        return new Task(() => write(amount, text));
+    }
+
+    static void write(int amount, string text)
+    {
+
+        for (int i = 0; i < amount; i++)
+        {
+            Console.WriteLine("" + (i + 1) + text);
+            Thread.Sleep(500);
+        }
+    }
+
+    static async void run()
+    {
+        Console.WriteLine("Hello, World!");
+        var t = GetTask(10, "peter");
+        t.Start();
+        Thread.Sleep(1000);
+        await t;
+        Console.WriteLine("After await");
+    }
+}
+
